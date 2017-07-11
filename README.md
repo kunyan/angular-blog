22c30611484c675ffcb9e14cf939ff57bc04efb7
Kun Yan <kyan@redhat.com>
2017-07-11 16:10:05 +0800

    Add README

---
+++ README.md
@@ -0,0 +1,23 @@
+### Angular Blog
+
+It is a demo project for mentorship program
+
+
+#### Getting Start
+
+```shell
+npm install
+npm run build # or gulp
+```
+
+if using [Yarn](https://yarnpkg.com/en/)
+```shell
+yarn install
+yarn run build
+```
+
+
+Open `dist/index.html` to browser to check update
+
+
+Will provide a local serve later
--- app/controllers/Article.js
+++ app/controllers/Article.js
@@ -2,10 +2,11 @@
 
 angular
   .module('blog')
-  .controller('Article', ['$scope', '$routeParams', function($scope, $routeParams) {
+  .controller('Article', ['$scope','$routeParams', function($scope, $routeParams) {
     $scope.article = {
         id: $routeParams.id,
         title: 'First Blog',
         content: 'Hello world',
+        author: 'Kun Yan',
       };
   }]);
